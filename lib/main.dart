05ba6be18160dce8463b127fcc8304df14cebd67
11Ramesh <140185843+11Ramesh@users.noreply.github.com>
2024-09-18 22:59:11 +0530

    commit 2

--- lib/main.dart
+++ lib/main.dart
@@ -57,6 +57,7 @@ class MyApp extends StatelessWidget {
     return BlocProvider(
       create: (context) => BackendBloc(),
       child: const MaterialApp(
+        debugShowCheckedModeBanner: false,
         home: Home(),
       ),
     );
